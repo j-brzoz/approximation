d1c1d918c00baccd89d0b95e94e7d7be438f0d85
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2023-01-19 18:05:25 +0100

    fix interpolator.c

--- interpolator.c
+++ interpolator.c
@@ -1,66 +1,55 @@
-#include "makespl.h"
-#include "piv_ge_solver.h"
-
 #include <stdio.h>
+#include "makespl.h"
+void
+make_spl (points_t * pts, spline_t * spl){
+    int i, j;
+	int n = pts->n;
+	n--;
+    
+	double h[n], A[n], l[n + 1], u[n + 1], z[n + 1], f[n], ff[n + 1], fff[n];
 
-void make_spl (points_t * pts, spline_t * spl)
-{
-
-	int n= pts->n - 1;
-	matrix_t *eqs= make_matrix( n*3, n*3+1 );
 	double *x = pts->x;
 	double *y = pts->y;
+	
+    for (i = 0; i <= n - 1; ++i) h[i] = x[i + 1] - x[i];
 
-	int i;
-
-	for( i= 0; i < n; i++ ) {
-		double dx= x[i+1] - x[i];
-		int if1= 3*i;
-		int if2= if1+1;
-		int if3= if2+1;
-		put_entry_matrix( eqs, if1, if1, dx );
-		put_entry_matrix( eqs, if1, if2, dx*dx/2 );
-		put_entry_matrix( eqs, if1, if3, dx*dx*dx/6 );
-		put_entry_matrix( eqs, if1, n*3, y[i+1]-y[i] );
-		put_entry_matrix( eqs, if2, if1, 1 );
-		put_entry_matrix( eqs, if2, if2, dx );
-		put_entry_matrix( eqs, if2, if3, dx*dx/2 );
-		if( if3+1 < n*3 )
-			put_entry_matrix( eqs, if2, if3+1, -1 );
-		else
-			put_entry_matrix( eqs, if2, if1, 0 );
-		put_entry_matrix( eqs, if3, if2, 1 );
-		put_entry_matrix( eqs, if3, if3, dx );
-		if( if3+2 < n*3 )
-			put_entry_matrix( eqs, if3, if3+2, -1 );
+    for (i = 1; i <= n - 1; ++i){
+        A[i] = 3 * (y[i + 1] - y[i]) / h[i] - 3 * (y[i] - y[i - 1]) / h[i - 1];
 	}
 
-#ifdef DEBUG
-	write_matrix( eqs, stdout );
-#endif
+    l[0] = 1;
+    u[0] = 0;
+    z[0] = 0;
 
-	if( piv_ge_solver( eqs ) ) {
-		spl->n = 0;
-		return;
-	}
+    for (i = 1; i <= n - 1; ++i) {
+        l[i] = 2 * (x[i + 1] - x[i - 1]) - h[i - 1] * u[i - 1];
+        u[i] = h[i] / l[i];
+        z[i] = (A[i] - h[i - 1] * z[i - 1]) / l[i];
+    }
 
-#ifdef DEBUG
-	write_matrix( eqs, stdout );
-#endif
+    l[n] = 1;
+    z[n] = 0;
+    ff[n] = 0;
 
-  if ( alloc_spl (spl, pts->n) == 0 ) {
-    spl->n = pts->n;
-		for( i= 0; i < n; i++ ) {
+    for (j = n - 1; j >= 0; --j) {
+        ff[j] = z[j] - u[j] * ff[j + 1];
+        f[j] = (y[j + 1] - y[j]) / h[j] - h[j] * (ff[j + 1] + 2 * ff[j]) / 3;
+        fff[j] = (ff[j + 1] - ff[j]) / (3 * h[j]);
+    }
+
+	spl->n = pts->n;
+	if ( alloc_spl (spl, pts->n) == 0 ) {
+		for (i = 0; i < n; ++i){
 			spl->x[i]= pts->x[i];
 			spl->f[i]= pts->y[i];
-			spl->f1[i]= get_entry_matrix( eqs, 3*i,   3*n );
-			spl->f2[i]= get_entry_matrix( eqs, 3*i+1, 3*n );
-			spl->f3[i]= get_entry_matrix( eqs, 3*i+2, 3*n );
+			spl->f1[i] = f[i];
+			spl->f2[i] = ff[i];
+			spl->f3[i] = fff[i];
 		}
 		spl->x[n]= pts->x[n];
-		spl->f[n]= pts->y[n];
-		spl->f1[n]= spl->f1[n-1];
-		spl->f2[n]= 0;
-		spl->f3[n]= 0;
-  }
+			spl->f[n]= pts->y[n];
+			spl->f1[n] = 1;
+			spl->f2[n] = 0;
+			spl->f3[n] = 0;
+	}
 }
